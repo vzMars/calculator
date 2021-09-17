d182bf63723d2a044c03c717ff8c592accc82ff8
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2021-09-17 18:13:17 -0400

    able to string together several operations and get right answer i just have to fix some other bugs

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,20 @@
-const numBtns = document.querySelectorAll('.numBtns');
+const topDisplay = document.querySelector('#top-display');
 const bottomDisplay = document.querySelector('#bottom-display');
+const numBtns = document.querySelectorAll('.numBtns');
+const operatorBtns = document.querySelectorAll('.operatorBtns');
+const equalBtn = document.querySelector('#equal');
 
-numBtns.forEach((numBtn) => numBtn.addEventListener('click', displayNum));
+let operationString = '';
+let numbers = [];
+let operator = '';
+let answer = '';
+
+numBtns.forEach((numBtn) => numBtn.addEventListener('click', displayNumber));
+operatorBtns.forEach((operatorBtn) =>
+  operatorBtn.addEventListener('click', displayOperator)
+);
+
+equalBtn.addEventListener('click', displayAnswer);
 
 function add(num1, num2) {
   return num1 + num2;
@@ -23,17 +36,102 @@ function operate(num1, num2, operator) {
   switch (operator) {
     case '+':
       return add(num1, num2);
-    case '-':
+    case '−':
       return subtract(num1, num2);
-    case '*':
+    case '×':
       return multiply(num1, num2);
-    case '/':
+    case '÷':
       return divide(num1, num2);
     default:
-      return 'Please use the correct operators';
+      return '';
   }
 }
 
-function displayNum(e) {
-  console.log(e.target.id);
+function displayAnswer() {
+  operator = operationString.replace(/[0-9]|\./g, '');
+  numbers = operationString.split(/\+|\−|\×|\÷/g);
+  console.log(operationString);
+  console.log(operator);
+  console.log(numbers);
+
+  answer = operate(parseFloat(numbers[0]), parseFloat(numbers[1]), operator);
+
+  topDisplay.innerText = answer;
+}
+
+function displayNumber(e) {
+  operationString += getChosenNum(e.target.id);
+  bottomDisplay.innerText = operationString;
+  enableOperatorBtns();
+}
+
+function displayOperator(e) {
+  if (
+    operationString.includes('+') ||
+    operationString.includes('−') ||
+    operationString.includes('×') ||
+    operationString.includes('÷')
+  ) {
+    displayAnswer();
+    operationString = answer;
+  }
+  operationString += getChosenOperator(e.target.id);
+  bottomDisplay.innerText = operationString;
+  disableOperatorBtns();
+}
+
+function getChosenNum(num) {
+  switch (num) {
+    case 'zero':
+      return '0';
+    case 'one':
+      return '1';
+    case 'two':
+      return '2';
+    case 'three':
+      return '3';
+    case 'four':
+      return '4';
+    case 'five':
+      return '5';
+    case 'six':
+      return '6';
+    case 'seven':
+      return '7';
+    case 'eight':
+      return '8';
+    case 'nine':
+      return '9';
+    case 'decimal':
+      return '.';
+    default:
+      return 'Something went wrong';
+  }
+}
+
+function getChosenOperator(operator) {
+  switch (operator) {
+    case 'add':
+      return '+';
+    case 'subtract':
+      return '−';
+    case 'multiply':
+      return '×';
+    case 'divide':
+      return '÷';
+    default:
+      return 'Something went wrong';
+  }
+}
+
+function enableOperatorBtns() {
+  for (let i = 0; i < operatorBtns.length; i++) {
+    operatorBtns[i].disabled = false;
+  }
+}
+
+function disableOperatorBtns() {
+  for (let i = 0; i < operatorBtns.length; i++) {
+    operatorBtns[i].disabled = true;
+  }
 }
